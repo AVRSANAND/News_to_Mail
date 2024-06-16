1b2a5e224b8ac6c9b19c0afcda897a5cf6021dae
AVRSANAND <avrsanand2003@gmail.com>
2024-06-16 17:07:07 +0530

    Minor error fixes and more dynamic

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import requests
 from send_mail import send_email
 api_key = "354d2a166c8d479bac7be4dafb38c860"
 
-topic = input("Enter a topic for the articles")
+topic = input("Enter a topic for the articles - ")
 
 url = (f"https://newsapi.org/v2/everything?q={topic}&"
-       "sortBy=popularity&"
+       "sortBy=publishedBy&"
        "apiKey=354d2a166c8d479bac7be4dafb38c860&"
        "language=en")
 
@@ -17,20 +17,23 @@ content = request.json()
 
 body = ""
 count = 0
-articles_num = int(input("How many articles do you want ?"))
+articles_num = int(input("How many articles do you want ? "))
+while articles_num > 50:
+    articles_num = int(input("Enter a number below 50! "))
+
 # Access the article titles and description
 for article in content["articles"]:
     if count < articles_num:
         if (article["title"] != "[Removed]") and (article["title"] is not None):
-            body = (body + article["title"] + "\n"
-                    + article["description"] + "\n" + article["url"] + 2*"\n")
-            body = "Subject: Today's News" \
+            body = (body + f"Title: {article['title']}" + "\n"
+                    + f"Desc: {article['description']}" + "\n" + f"Link: {article['url']}" + 2*"\n")
+            body = f"Subject: Today's News about {topic}" \
             + "\n" + body
         else:
             continue
         count += 1
     else:
         break
+
 body = body.encode("utf-8")
-print(body)
 send_email(body)
